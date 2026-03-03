926eb53967b7226a2c50c264475a12f501bd29d5
roncofaber <fabrice.roncoroni@gmail.com>
2026-03-02 17:04:10 -0800

    Last small changes before new release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "crucible.lens"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "crucible.lens"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "0.2.0"
 
