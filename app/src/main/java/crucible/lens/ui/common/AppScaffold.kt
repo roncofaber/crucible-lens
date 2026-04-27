97a2fed5d6d6efe8099037586c8e08be90c3e606
roncofaber <fabrice.roncoroni@gmail.com>
2026-04-26 21:04:17 -0700

    Fix missing calculateStartPadding/calculateEndPadding imports in AppScaffold

--- app/src/main/java/crucible/lens/ui/common/AppScaffold.kt
+++ app/src/main/java/crucible/lens/ui/common/AppScaffold.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
