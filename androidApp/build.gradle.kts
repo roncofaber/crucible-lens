3179ee7eaa1b47d831031c282c5f9f6b58f9e33e
roncofaber <fabrice.roncoroni@gmail.com>
2026-05-07 10:49:36 -0700

    Bump version to 0.4.0 (versionCode 5)

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "crucible.lens"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.3.0"
+        versionCode = 5
+        versionName = "0.4.0"
     }
 
     signingConfigs {
